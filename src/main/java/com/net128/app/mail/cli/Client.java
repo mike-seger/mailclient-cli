4fec2c4ddd1b6a7eaa6a793a8e842f70d522a21b
Michael Seger <michael@net128.com>
2024-02-13 10:13:25 +0100

    cleanup

--- src/main/java/com/net128/app/mail/cli/Client.java
+++ src/main/java/com/net128/app/mail/cli/Client.java
@@ -10,12 +10,11 @@ import jakarta.mail.MessagingException;
 public class Client {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Usage: java Client [boxes|messages <box-name>|show <box-name> <message-number>]");
+            System.out.println("Usage: java Client [boxes|messages|show|delete|send] ...");
             return;
         }
         
-        // Load properties from imap.properties
-        Properties props = new Properties();
+        var props = new Properties();
         try (InputStream input = new FileInputStream("mail.properties")) {
             props.load(input);
         } catch (IOException ex) {
@@ -23,41 +22,35 @@ public class Client {
             return;
         }
 
-        String user = props.getProperty("user");
-        String password = props.getProperty("password");
-        String imapHost = props.getProperty("imap-host");
-        String imapPort = props.getProperty("imap-port");
+        var user = props.getProperty("user");
+        var password = props.getProperty("password");
+        var imapHost = props.getProperty("imap-host");
+        var imapPort = props.getProperty("imap-port");
 
-        IMAPService imapService = new IMAPService(user, password, imapHost, imapPort);
-
-        String smtpHost = props.getProperty("smtp-host");
-        String smtpPort = props.getProperty("smtp-port");
-
-        SMTPService smtpService = new SMTPService(smtpHost, smtpPort, user, password, false);
+        var imapService = new IMAPService(user, password, imapHost, imapPort);
+        var smtpHost = props.getProperty("smtp-host");
+        var smtpPort = props.getProperty("smtp-port");
+        var smtpService = new SMTPService(smtpHost, smtpPort, user, password, false);
 
         try {
+            if(!"send".equals(args[0])) imapService.connect();
             switch (args[0]) {
                 case "boxes":
-                    imapService.connect();
-                    List<String> mailboxes = imapService.listMailboxes();
-                    mailboxes.forEach(System.out::println);
+                    imapService.listMailboxes().forEach(System.out::println);
                     break;
                 case "messages":
                     if (args.length < 2) {
                         System.out.println("Usage: java Client messages <box-name>");
                         return;
                     }
-                    imapService.connect();
-                    List<String> messages = imapService.listMessages(args[1]);
-                    messages.forEach(System.out::println);
+                    imapService.listMessages(args[1]).forEach(System.out::println);
                     break;
                 case "show":
                     if (args.length < 3) {
                         System.out.println("Usage: java Client show <box-name> <message-number>");
                         return;
                     }
-                    imapService.connect();
-                    String content = imapService.showMessage(Integer.parseInt(args[2]), args[1]);
+                    var content = imapService.showMessage(Integer.parseInt(args[2]), args[1]);
                     System.out.println(content);
                     break;
                 case "delete":
@@ -65,8 +58,7 @@ public class Client {
                         System.out.println("Usage: java Client delete <box-name> <message-number>");
                         return;
                     }
-                    imapService.connect();
-                    boolean result = imapService.deleteMessage(Integer.parseInt(args[2]), args[1]);
+                    var result = imapService.deleteMessage(Integer.parseInt(args[2]), args[1]);
                     if (result) {
                         System.out.println("Message deleted successfully.");
                     } else {
@@ -78,11 +70,7 @@ public class Client {
                         System.out.println("Usage: java Client send <from> <to> <subject> <body>");
                         return;
                     }
-                    String from = args[1];
-                    String to = args[2];
-                    String subject = args[3];
-                    String body = args[4];
-                    smtpService.sendMessage(from, to, subject, body);
+                    smtpService.sendMessage(args[1], args[2], args[3], args[4]);
                     System.out.println("Message sent successfully.");
                     break;
                 default:
